3aa1d6314eea4a04762a92f999b907119ca67ba2
syr9707 <yrshin9771@gmail.com>
2023-02-13 17:06:01 +0900

    test(passwordEncoder) : 비밀번호 암호화 테스트

--- src/test/java/com/joinproject/learning/PasswordEncoderTest.java
+++ src/test/java/com/joinproject/learning/PasswordEncoderTest.java
@@ -1,2 +1,30 @@
-package com.joinproject.learning;public class PasswordEncoderTest {
+package com.joinproject.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class PasswordEncoderTest {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    void 패스워드_암호화() throws Exception {
+        // given
+        String password = "ummchicken";
+
+        // when
+        String encodePassword = passwordEncoder.encode(password);
+
+        // then
+        assertThat(encodePassword).startsWith("{");
+        assertThat(encodePassword).contains("{bcrypt}");
+        assertThat(encodePassword).isNotEqualTo(password);
+    }
+
 }
