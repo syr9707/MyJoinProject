2b8177e65f4482e1289d318dfce14a16252523ff
syr9707 <yrshin9771@gmail.com>
2023-02-13 13:54:11 +0900

    first commit test

---
+++ src/main/java/com/joinproject/JoinprojectApplication.java
@@ -0,0 +1,13 @@
+package com.joinproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joinproject/JoinprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
