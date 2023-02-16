cd62811cb1e10cd29619074a8fb4e1bbb5966f4f
syr9707 <yrshin9771@gmail.com>
2023-02-16 23:14:13 +0900

    feat(Member Service) : 회원가입 로직 생성

--- src/main/java/com/joinproject/domain/member/service/MemberService.java
+++ src/main/java/com/joinproject/domain/member/service/MemberService.java
@@ -1,2 +1,24 @@
-package com.joinproject.domain.member.service;public interface MemberService {
+package com.joinproject.domain.member.service;
+
+import com.joinproject.domain.member.dto.MemberInfoDto;
+import com.joinproject.domain.member.dto.MemberSignUpDto;
+import com.joinproject.domain.member.dto.MemberUpdateDto;
+
+public interface MemberService {
+
+    /**
+     * 회원가입 CRUD
+     * */
+    void signUp(MemberSignUpDto memberSignUpDto) throws Exception;
+
+    void update(MemberUpdateDto memberUpdateDto) throws Exception;
+
+    void updatePassword(String checkPassword, String toBePassword) throws Exception;
+
+    void withdraw(String checkPassword) throws Exception;
+
+    MemberInfoDto getInfo(Long id) throws Exception;
+
+    MemberInfoDto getMyInfo() throws Exception;
+
 }
--- src/main/java/com/joinproject/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/joinproject/domain/member/service/MemberServiceImpl.java
@@ -1,2 +1,103 @@
-package com.joinproject.domain.member.service;public class MemberServiceImpl {
+package com.joinproject.domain.member.service;
+
+import com.joinproject.domain.member.Member;
+import com.joinproject.domain.member.dto.MemberInfoDto;
+import com.joinproject.domain.member.dto.MemberSignUpDto;
+import com.joinproject.domain.member.dto.MemberUpdateDto;
+import com.joinproject.domain.member.repository.MemberRepository;
+import com.joinproject.global.security.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void signUp(MemberSignUpDto memberSignUpDto) throws Exception {
+        Member member = memberSignUpDto.toEntity();
+        member.addUserAuthority();
+        member.encodePassword(passwordEncoder);
+
+        if (memberRepository.findByUsername(memberSignUpDto.getUsername()).isPresent()) {
+            throw new Exception("이미 존재하는 아이디입니다.");
+        }
+
+        memberRepository.save(member);
+    }
+
+    @Override
+    public void update(MemberUpdateDto memberUpdateDto) throws Exception {
+        Member member = memberRepository.findByUsername(SecurityUtil.getLoginUserName()).orElseThrow(
+                () -> new Exception("회원이 존재하지 않습니다.")
+        );
+
+        memberUpdateDto.getAge().ifPresent(member::updateAge);
+        memberUpdateDto.getName().ifPresent(member::updateName);
+        memberUpdateDto.getNickname().ifPresent(member::updateNickName);
+    }
+
+    @Override
+    public void updatePassword(String checkPassword, String toBePassword) throws Exception {
+        Member member = memberRepository.findByUsername(SecurityUtil.getLoginUserName()).orElseThrow(
+                () -> new Exception("회원이 존재하지 않습니다.")
+        );
+
+        if(!member.matchPassword(passwordEncoder, checkPassword) ) {
+            throw new Exception("비밀번호가 일치하지 않습니다.");
+        }
+
+        member.updatePassword(passwordEncoder, toBePassword);
+    }
+
+
+    /**
+     * 회원 탈퇴
+     * */
+    @Override
+    public void withdraw(String checkPassword) throws Exception {
+        Member member = memberRepository.findByUsername(SecurityUtil.getLoginUserName()).orElseThrow(
+                () -> new Exception("회원이 존재하지 않습니다.")
+        );
+
+        if(!member.matchPassword(passwordEncoder, checkPassword) ) {
+            throw new Exception("비밀번호가 일치하지 않습니다.");
+        }
+
+        memberRepository.delete(member);
+    }
+
+
+    /**
+     * id를 받아와서 해당 회원 정보 조회
+     * */
+    @Override
+    public MemberInfoDto getInfo(Long id) throws Exception {
+        Member findMember = memberRepository.findById(id).orElseThrow(
+                () -> new Exception("회원이 없습니다.")
+        );
+
+        return new MemberInfoDto(findMember);
+    }
+
+    /**
+     * 나의 정보 가져오기
+     *
+     * 로그인 한 경우, SecurityContextHolder에 들어있기 때문에 따로 입력받지 않아도
+     * 인증만 되어있다면 정보 조회 가능
+     * */
+    @Override
+    public MemberInfoDto getMyInfo() throws Exception {
+        Member findMember = memberRepository.findByUsername(SecurityUtil.getLoginUserName()).orElseThrow(
+                () -> new Exception("회원이 존재하지 않습니다.")
+        );
+
+        return new MemberInfoDto(findMember);
+    }
 }
