c20f9294450e88827040daaade4ecc187ce343a2
syr9707 <yrshin9771@gmail.com>
2023-02-17 10:08:33 +0900

    feat(MemberException) : 회원 예외처리 생성

--- src/main/java/com/joinproject/domain/member/exception/MemberException.java
+++ src/main/java/com/joinproject/domain/member/exception/MemberException.java
@@ -1,2 +1,18 @@
-package com.joinproject.domain.member.exception;public class MemberException {
+package com.joinproject.domain.member.exception;
+
+import com.joinproject.global.exception.BaseException;
+import com.joinproject.global.exception.BaseExceptionType;
+
+public class MemberException extends BaseException {
+
+    private BaseExceptionType exceptionType;
+
+    public MemberException(BaseExceptionType exceptionType) {
+        this.exceptionType = exceptionType;
+    }
+
+    @Override
+    public BaseExceptionType getExceptionType() {
+        return exceptionType;
+    }
 }
