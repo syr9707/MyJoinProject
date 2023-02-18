fb620fdf36c5a73d8335d7fb22210398ad606102
syr9707 <yrshin9771@gmail.com>
2023-02-18 20:29:55 +0900

    feat(Exception) : 예외 발생 HTTP 상태코드 변경

--- src/main/java/com/joinproject/global/exception/ExceptionAdvice.java
+++ src/main/java/com/joinproject/global/exception/ExceptionAdvice.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,20 +16,39 @@ public class ExceptionAdvice {
 
     /**
      * 서버에서 예외가 발생하더라도, 상태코드는 200을 반환하도록 설정
+     *
+     * HttpMessageNotReadableException  => json 파싱 오류
      * */
 
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity handleBaseEx(BaseException exception) {
+    public ResponseEntity handleBaseEx(BaseException exception){
         log.error("BaseException errorMessage(): {}",exception.getExceptionType().getErrorMessage());
         log.error("BaseException errorCode(): {}",exception.getExceptionType().getErrorCode());
 
         return new ResponseEntity(new ExceptionDto(exception.getExceptionType().getErrorCode()),exception.getExceptionType().getHttpStatus());
     }
 
+    // @Valid에서 예외 발생
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity handleValidEx(BindException exception){
+
+        log.error("@ValidException 발생! {}", exception.getMessage() );
+        return new ResponseEntity(new ExceptionDto(2000),HttpStatus.BAD_REQUEST);
+    }
+
+    //HttpMessageNotReadableException  => json 파싱 오류
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity httpMessageNotReadableExceptionEx(HttpMessageNotReadableException exception){
+
+        log.error("Json을 파싱하는 과정에서 예외 발생! {}", exception.getMessage() );
+        return new ResponseEntity(new ExceptionDto(3000),HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity handleMemberEx(Exception exception) {
+
         exception.printStackTrace();
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
     @Data
