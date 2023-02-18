f1b248093770f65f0372cdb3045119a778339c3b
syr9707 <yrshin9771@gmail.com>
2023-02-18 21:23:48 +0900

    feat(File Exception) : File Custom Exception 생성

--- src/main/java/com/joinproject/global/file/exception/FileException.java
+++ src/main/java/com/joinproject/global/file/exception/FileException.java
@@ -1,2 +1,18 @@
-package com.joinproject.global.file.exception;public class FileException {
+package com.joinproject.global.file.exception;
+
+import com.joinproject.global.exception.BaseException;
+import com.joinproject.global.exception.BaseExceptionType;
+
+public class FileException extends BaseException {
+
+    private BaseExceptionType exceptionType;
+
+    public FileException(BaseExceptionType exceptionType) {
+        this.exceptionType = exceptionType;
+    }
+
+    @Override
+    public BaseExceptionType getExceptionType() {
+        return exceptionType;
+    }
 }
--- src/main/java/com/joinproject/global/file/service/FileService.java
+++ src/main/java/com/joinproject/global/file/service/FileService.java
@@ -1,2 +1,15 @@
-package com.joinproject.global.file.service;public interface FileService {
+package com.joinproject.global.file.service;
+
+import com.joinproject.global.file.exception.FileException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+
+public interface FileService {
+
+    //저장된 파일 경로 반환
+    String save(MultipartFile multipartFile) throws FileException;
+
+    void delete(String filePath);
+
 }
