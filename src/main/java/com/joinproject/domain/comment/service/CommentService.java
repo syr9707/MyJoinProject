b452df77a0881ce8106f91b9773201fb3df5deea
syr9707 <yrshin9771@gmail.com>
2023-02-22 21:08:51 +0900

    feat(CommentService) : 댓글 & 대댓글 Service 코드 수정

--- src/main/java/com/joinproject/domain/comment/service/CommentService.java
+++ src/main/java/com/joinproject/domain/comment/service/CommentService.java
@@ -1,14 +1,19 @@
 package com.joinproject.domain.comment.service;
 
 import com.joinproject.domain.comment.Comment;
+import com.joinproject.domain.comment.dto.CommentSaveDto;
+import com.joinproject.domain.comment.dto.CommentUpdateDto;
+import com.joinproject.domain.comment.exception.CommentException;
 
 import java.util.List;
 
 public interface CommentService {
 
-    void save(Comment comment);
-    Comment findById(Long id) throws Exception;
-    List<Comment> findAll();
-    void remove(Long id) throws Exception;
+    void save(Long postId , CommentSaveDto commentSaveDto);
+    void saveReComment(Long postId, Long parentId ,CommentSaveDto commentSaveDto);
+
+    void update(Long id, CommentUpdateDto commentUpdateDto);
+
+    void remove(Long id) throws CommentException;
 
 }
--- src/main/java/com/joinproject/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/joinproject/domain/comment/service/CommentServiceImpl.java
@@ -1,7 +1,18 @@
 package com.joinproject.domain.comment.service;
 
 import com.joinproject.domain.comment.Comment;
+import com.joinproject.domain.comment.dto.CommentSaveDto;
+import com.joinproject.domain.comment.dto.CommentUpdateDto;
+import com.joinproject.domain.comment.exception.CommentException;
+import com.joinproject.domain.comment.exception.CommentExceptionType;
 import com.joinproject.domain.comment.repository.CommentRepository;
+import com.joinproject.domain.member.exception.MemberException;
+import com.joinproject.domain.member.exception.MemberExceptionType;
+import com.joinproject.domain.member.repository.MemberRepository;
+import com.joinproject.domain.post.exception.PostException;
+import com.joinproject.domain.post.exception.PostExceptionType;
+import com.joinproject.domain.post.repository.PostRepository;
+import com.joinproject.global.security.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,33 +25,69 @@ import java.util.List;
 public class CommentServiceImpl implements CommentService{
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+
 
     @Override
-    public void save(Comment comment) {
+    public void save(Long postId, CommentSaveDto commentSaveDto) {
+        Comment comment = commentSaveDto.toEntity();
+
+        comment.confirmWriter(memberRepository.findByUsername(SecurityUtil.getLoginUserName()).orElseThrow(
+                () -> new MemberException(MemberExceptionType.NOT_FOUND_MEMBER))
+        );
+
+        comment.confirmPost(postRepository.findById(postId).orElseThrow(
+                () -> new PostException(PostExceptionType.POST_NOT_POUND))
+        );
+
         commentRepository.save(comment);
     }
 
-    @Transactional(readOnly = true)
     @Override
-    public Comment findById(Long id) throws Exception {
-        return commentRepository.findById(id).orElseThrow(
-                () -> new Exception("댓글이 없습니다.")
+    public void saveReComment(Long postId, Long parentId, CommentSaveDto commentSaveDto) {
+        Comment comment = commentSaveDto.toEntity();
+
+        comment.confirmWriter(memberRepository.findByUsername(SecurityUtil.getLoginUserName()).orElseThrow(
+                () -> new MemberException(MemberExceptionType.NOT_FOUND_MEMBER))
         );
+
+        comment.confirmPost(postRepository.findById(postId).orElseThrow(
+                () -> new PostException(PostExceptionType.POST_NOT_POUND))
+        );
+
+        comment.confirmParent(commentRepository.findById(parentId).orElseThrow(
+                () -> new CommentException(CommentExceptionType.NOT_POUND_COMMENT))
+        );
+
+        commentRepository.save(comment);
     }
 
-    @Transactional(readOnly = true)
     @Override
-    public List<Comment> findAll() {
-        return commentRepository.findAll();
-    }
-
-    @Override
-    public void remove(Long id) throws Exception {
+    public void update(Long id, CommentUpdateDto commentUpdateDto) {
         Comment comment = commentRepository.findById(id).orElseThrow(
-                () -> new Exception("댓글이 없습니다.")
+                () -> new CommentException(CommentExceptionType.NOT_POUND_COMMENT)
         );
+
+        if(!comment.getWriter().getUsername().equals(SecurityUtil.getLoginUserName())){
+            throw new CommentException(CommentExceptionType.NOT_AUTHORITY_UPDATE_COMMENT);
+        }
+
+        commentUpdateDto.getContent().ifPresent(comment::updateContent);
+    }
+
+    @Override
+    public void remove(Long id) throws CommentException {
+        Comment comment = commentRepository.findById(id).orElseThrow(
+                () -> new CommentException(CommentExceptionType.NOT_POUND_COMMENT)
+        );
+
+        if(!comment.getWriter().getUsername().equals(SecurityUtil.getLoginUserName())){
+            throw new CommentException(CommentExceptionType.NOT_AUTHORITY_DELETE_COMMENT);
+        }
+
         comment.remove();
         List<Comment> removableCommentList = comment.findRemovableList();
-        removableCommentList.forEach(removableComment -> commentRepository.delete(removableComment));
+        commentRepository.deleteAll(removableCommentList);
     }
 }
