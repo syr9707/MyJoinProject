2ef71dc5b771ffd3df6b1de5b6d230d0a20c0407
syr9707 <yrshin9771@gmail.com>
2023-02-22 21:09:53 +0900

    feat(CommentException) : 댓글 Custom Exception 생성
    - 댓글 수정 & 삭제 권한

--- src/main/java/com/joinproject/domain/comment/exception/CommentException.java
+++ src/main/java/com/joinproject/domain/comment/exception/CommentException.java
@@ -1,2 +1,18 @@
-package com.joinproject.domain.comment.exception;public class CommentException {
+package com.joinproject.domain.comment.exception;
+
+import com.joinproject.global.exception.BaseException;
+import com.joinproject.global.exception.BaseExceptionType;
+
+public class CommentException extends BaseException {
+
+    private BaseExceptionType baseExceptionType;
+
+    public CommentException(BaseExceptionType baseExceptionType) {
+        this.baseExceptionType = baseExceptionType;
+    }
+
+    @Override
+    public BaseExceptionType getExceptionType() {
+        return this.baseExceptionType;
+    }
 }
